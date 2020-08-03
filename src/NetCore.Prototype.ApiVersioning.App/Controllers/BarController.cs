3fb3757373d4470e2ca64d3ec22836aef1fdfbc3
Dimitrie Tataru <dimitrie.tataru@assist.ro>
2020-08-03 12:49:49 +0300

    Add API endpoints with hardcoded versioning

--- src/NetCore.Prototype.ApiVersioning.App/Controllers/BarController.cs
+++ src/NetCore.Prototype.ApiVersioning.App/Controllers/BarController.cs
@@ -2,7 +2,22 @@
 
 namespace NetCore.Prototype.ApiVersioning.App.Controllers
 {
+    [ApiController]
+    [Produces("application/json")]
     public sealed class BarController : ControllerBase
     {
+        [HttpGet]
+        [Route("api/v1/bar/test-one")]
+        public IActionResult TestOne()
+        {
+            return NoContent();
+        }
+
+        [HttpGet]
+        [Route("api/v2/foo/test-two")]
+        public IActionResult TestTwo()
+        {
+            return NoContent();
+        }
     }
 }
--- src/NetCore.Prototype.ApiVersioning.App/Controllers/FooController.cs
+++ src/NetCore.Prototype.ApiVersioning.App/Controllers/FooController.cs
@@ -2,7 +2,15 @@
 
 namespace NetCore.Prototype.ApiVersioning.App.Controllers
 {
+    [ApiController]
+    [Produces("application/json")]
     public sealed class FooController : ControllerBase
     {
+        [HttpGet]
+        [Route("api/v1/foo/test")]
+        public IActionResult Test()
+        {
+            return NoContent();
+        }
     }
 }
